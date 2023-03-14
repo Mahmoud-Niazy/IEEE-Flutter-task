a90401aeba2e879320384ad9e3f27392fb40a771
Mahmoud-Niazy <mahmoudniazy33@gmail.com>
2023-03-15 01:16:32 +0200

    Task

---
+++ code.dart
@@ -0,0 +1,12 @@
+
+import 'dart:io';
+
+void main() {
+  print('Enter the number') ;
+  int number = int.parse(stdin.readLineSync()!);
+  for (var i = 1; i <= number; i++) {
+    if (number % i == 0) {
+      print(i);
+    }
+  }
+}
